45e1b94b27573b2ed623b1ae745feb256eeea962
Stanislsav <Sculkoskij@gmail.ru>
2023-03-29 19:24:02 +0300

    Решил всё

---
+++ Exsample1/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32 (Console.ReadLine());
+int count = 1;
+GetNumber (N, count);
+
+
+void GetNumber (int n, int x)
+{
+    if (x > n) return;
+    GetNumber (n, x+1);
+    Console.Write(x + " ");
+}
---
+++ Exsample3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+
+Console.WriteLine("Введите неотрицательное число M");
+int M = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите неотрицательное число N");
+int N = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine(Akkerman(M,N));
+
+int Akkerman (int m, int n)
+{
+    if (m == 0) 
+    {
+        return n+1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return Akkerman(m-1, 1);
+    }
+    else
+    {
+    return Akkerman(m-1, Akkerman(m, n - 1));
+    }
+}
